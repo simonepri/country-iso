3c9c04fbc6ee87bfe52d7a8dcf3b199dc6b28fba
chqma <chq.matteo@gmail.com>
2017-09-04 15:51:42 +0200

    Add getCountry function

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
+
+const PolygonLookup = require('polygon-lookup');
+
+let worldGeojson = undefined;
+let worldLookup = undefined;
+
 /**
  * Use geojson for searches
  * @param  {[type]} geojson a FeatureCollection, each feature must have a 'ISO_A3' as property
@@ -9,3 +15,15 @@ module.exports.use = function (geojson) {
   worldLookup = new PolygonLookup(worldGeojson);
 };
 
+/**
+ * @param {LatLng} latlng
+ * @returns ISO 3166 alpha-3 country code for geographic coordinates
+ */
+module.exports.getCountry = function (latlng) {
+  return new Promise((resolve, reject) => {
+    if (worldGeojson === undefined) {
+      reject(new Error('No geographical data loaded'));
+    }
+    resolve(worldLookup.search(latlng.lng, latlng.lat));
+  });
+};
